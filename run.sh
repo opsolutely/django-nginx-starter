750895017891b67dca3d546ca449e0ee3bfbe098
Kate Heddleston <kate.heddleston@gmail.com>
2012-11-11 13:00:47 -0800

    Add run.sh file.

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# find our way to the "app" dir where run.sh lives so we can set up paths
+pushd `dirname "$0"` &> /dev/null
+
+USER_SETTINGS=$USER
+export DJANGO_SETTINGS_MODULE=app.settings
+export REDIS_URL='redis://localhost:6379'
+export ENVIRONMENT='dev'
+
+source keys.sh
+
+# get ourselves back to the beginning so that the script path isn't relative to app, but relative to where we ran the command
+popd &> /dev/null
+
+command=$1
+shift # rather than trying to predict the total # of arguments, lop off the command argument and invoke the rest of the arguments
+python $command "$@"
